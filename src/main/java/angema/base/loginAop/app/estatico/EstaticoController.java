f06a41d49384aa6f1fdc0b91679d99d3c7de0155
infoangema <info@angema.com.ar>
2022-09-28 16:02:36 -0300

    [v.4][CHANGE][DEVS](GERD): - Todo para EstaticoController.

---
+++ src/main/java/angema/base/loginAop/app/estatico/EstaticoController.java
@@ -0,0 +1,13 @@
+package angema.base.loginAop.app.estatico;
+
+import org.springframework.stereotype.Controller;
+
+@Controller("/socio")
+public class EstaticoController {
+
+    // todo getLogoByCuitSocio
+
+    // todo getCorrouselByCiutSocio
+
+    // todo getFontByCuitSocio
+}
--- src/main/java/angema/base/loginAop/app/tematica/TematicaController.java
+++ src/main/java/angema/base/loginAop/app/tematica/TematicaController.java
@@ -13,20 +13,20 @@ public class TematicaController {
     @Autowired
     private TematicaService tematicaService;
 
-    @GetMapping("/tematica")
+    @GetMapping("/tematica/{cuit}")
     public String getTematica() {
         JsonObject json = new JsonObject();
         // put some value pairs into the JSON object .
         json.addProperty("colorPrimary", "blue");
         json.addProperty("colorSecondary", "lightcoral");
-        json.addProperty("fontPrimary", "Times New Roman");
+        json.addProperty("fontPrimary", "http://localhost:8080/socio/30717119238/theme/font/eemplo.tiff");
+        json.addProperty("logo", "http://localhost:8080/socio/30717119238/theme/imagen/logo/logo-carre.png");
 
         String resultado=json.toString();
         return resultado;
 
 
     }
-
 }
 
 
