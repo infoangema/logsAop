3bbc5e5ea9df1a574687e7ca27dfe744d21e1886
infoangema <info@angema.com.ar>
2022-09-28 16:01:56 -0300

    [v.4][CHANGE][DEVS](GERD): - Borrada clase Employee.

--- src/main/java/angema/base/loginAop/app/registros/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package angema.base.loginAop.app.registros;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository < Employee, Long > {
-
-}
