b075891b20c73972080b7c7dfe80a147271cd2d8
Fer-Barragan <barragan.fer10@gmail.com>
2022-09-29 11:43:14 -0300

    [v.0][t.1.0][CODE][ECOMMERCE](FERBA): Se solucionó el tema de los componentes Nabvar y Futter que no los reconocia el Dashboard

--- src/main/java/angema/base/loginAop/app/tematica/TematicaController.java
+++ src/main/java/angema/base/loginAop/app/tematica/TematicaController.java
@@ -9,19 +9,30 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:4200")
 public class TematicaController {
+    public static final String cuitCarre = "30687310434";
+    public static final String cuitMusimundo = "30697265895";
 
     @Autowired
     private TematicaService tematicaService;
 
     @GetMapping("/tematica/{cuit}")
-    public String getTematica() {
+    public String getTematica(@PathVariable(value = "cuit") String cuit) {
         JsonObject json = new JsonObject();
         // put some value pairs into the JSON object .
-        json.addProperty("colorPrimary", "blue");
-        json.addProperty("colorSecondary", "lightcoral");
-        json.addProperty("fontPrimary", "http://localhost:8080/socio/30717119238/theme/font/eemplo.tiff");
-        json.addProperty("logo", "http://localhost:8080/socio/30717119238/theme/imagen/logo/logo-carre.png");
-
+        if(cuit.equals(cuitCarre)){
+            json.addProperty("colorPrimary", "#0093d3");
+            json.addProperty("colorSecondary", "#e30512");
+            json.addProperty("fontPrimary", "http://localhost:8080/socio/30717119238/theme/font/eemplo.tiff");
+            //json.addProperty("logo", "http://localhost:8080/socio/30717119238/theme/imagen/logo/logo-carre.png");
+            json.addProperty("logo", " https://www.servicioscarrefour1.com.ar/image/layout_set_logo?img_id=23766&t=1661563261880");
+        }
+        else{
+            json.addProperty("colorPrimary", "#e30613");
+            json.addProperty("colorSecondary", "#e30613");
+            json.addProperty("fontPrimary", "http://localhost:8080/socio/30717119238/theme/font/eemplo.tiff");
+            //json.addProperty("logo", "http://localhost:8080/socio/30717119238/theme/imagen/logo/logo-carre.png");
+            json.addProperty("logo", "https://mimundoprotegido.com.ar/assets/images/logo.png");
+        }
         String resultado=json.toString();
         return resultado;
 
