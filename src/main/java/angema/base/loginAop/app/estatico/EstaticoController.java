5225adbac00063b47a6f77fdeec218924f6a07d2
Fer-Barragan <barragan.fer10@gmail.com>
2022-09-29 17:42:31 -0300

    [v.0][t.1.0][FEAT][ECOMMERCE](FERBA): Se creó metodo para obtener el logo de manera estatica a traves de un servicio

--- src/main/java/angema/base/loginAop/app/estatico/EstaticoController.java
+++ src/main/java/angema/base/loginAop/app/estatico/EstaticoController.java
@@ -1,11 +1,35 @@
 package angema.base.loginAop.app.estatico;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@Controller("/socio")
+import java.io.IOException;
+import java.io.InputStream;
+
+
+@RequestMapping("/socio")
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class EstaticoController {
 
     // todo getLogoByCuitSocio
+
+ //   @GetMapping("/logo")
+//    public static String getLogo() {
+//       String logoUrl="/src/main/resources/static/images/layout_set_logo.png";
+//        return logoUrl;
+//
+//
+//    }
+    @GetMapping(value = "/logo")
+    public @ResponseBody byte[] getLogo() throws IOException {
+        InputStream in = getClass()
+                .getResourceAsStream("/src/main/resources/static/images/layout_set_logo.png");
+        return org.apache.commons.io.IOUtils.toByteArray(in);
+    }
+
+
 
     // todo getCorrouselByCiutSocio
 
