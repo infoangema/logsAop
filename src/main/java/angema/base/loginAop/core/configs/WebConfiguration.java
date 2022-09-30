82124ebbe66d4ac8fb665d67b1a45f71a38532fe
Fer-Barragan <barragan.fer10@gmail.com>
2022-09-30 12:35:30 -0300

    [v.0][t.1.0][FEAT][ECOMMERCE](FERBA): Nueva clase que acepta la configuracion de componentes estaticos

---
+++ src/main/java/angema/base/loginAop/core/configs/WebConfiguration.java
@@ -0,0 +1,13 @@
+package angema.base.loginAop.core.configs;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+public class WebConfiguration extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
