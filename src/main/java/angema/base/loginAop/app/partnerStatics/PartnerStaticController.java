65715f266abc7ffd59da9cced14c2caf56075b6c
infoangema <info@angema.com.ar>
2022-09-30 14:20:17 -0300

    [v.4][SET][DEVS](GERD): - Borrado PartnerStaticController.

--- src/main/java/angema/base/loginAop/app/partnerStatics/PartnerStaticController.java
+++ src/main/java/angema/base/loginAop/app/partnerStatics/PartnerStaticController.java
@@ -1,32 +1,22 @@
 package angema.base.loginAop.app.partnerStatics;
 
-import org.apache.commons.io.IOUtils;
-
 import org.springframework.http.MediaType;
-import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import org.apache.commons.io.IOUtils;
 import java.io.IOException;
 import java.io.InputStream;
 
-
-@RequestMapping("/socio")
+@RequestMapping("/statics")
 @RestController
-public class EstaticoController {
-    public static final String cuitCarre = "30687310434";
-    // todo getLogoByCuitSocio
-
-
+public class PartnerStaticController {
 
     @GetMapping(value="/logo/{cuit_socio}",produces = MediaType.IMAGE_PNG_VALUE)
     @ResponseBody
     public  byte[] getLogo(@PathVariable String cuit_socio) throws IOException {
-        InputStream in = EstaticoController.class.getResourceAsStream("/static/images/logo-"+cuit_socio+".png");
-
+        InputStream in = PartnerStaticController.class.getResourceAsStream("/static/images/logo-"+cuit_socio+".png");
         return IOUtils.toByteArray(in);
     }
-
 
     // todo getCorrouselByCiutSocio
 
