cd880628b97d75f22a2c5c5322427a045ece0e4b
infoangema <info@angema.com.ar>
2022-10-08 22:36:03 -0300

    [v.0][t.5.0][CHANGE][DEVS](GERD): - Mock de productos desde archivo json.

---
+++ src/main/java/angema/base/loginAop/core/utils/FileSystemUtil.java
@@ -0,0 +1,23 @@
+package angema.base.loginAop.core.utils;
+
+import angema.base.loginAop.app.partnerStatics.PartnerStaticController;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+@Service
+public class FileSystemUtil {
+    Gson gson = new Gson();
+
+    public String getFile(String path) {
+        InputStream in = FileSystemUtil.class.getResourceAsStream(path);
+        JsonElement json = gson.fromJson(new InputStreamReader(in, Charset.forName("UTF-8").newDecoder()), JsonElement.class);
+        String jsonStr = ((JsonObject) json).get("body").toString();
+        return jsonStr;
+    }
+}
