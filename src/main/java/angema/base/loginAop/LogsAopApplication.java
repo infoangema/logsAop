f83e87f61222c3f785e337d4e8c9e6be82358e8e
Fer-Barragan <barragan.fer10@gmail.com>
2022-10-17 09:36:54 -0300

    [v.0][t.9.0][FEAT][ECOMMERCE](FERBA): Se obtuvo desde la clase principal todas las variables que se aplicacion en el properties

--- src/main/java/angema/base/loginAop/LogsAopApplication.java
+++ src/main/java/angema/base/loginAop/LogsAopApplication.java
@@ -2,24 +2,43 @@ package angema.base.loginAop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 @CrossOrigin
 @EnableConfigurationProperties
 @SpringBootApplication
 public class LogsAopApplication {
+
+
+
+
+
 
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
 	}
 
+	@Bean
+	@ConfigurationProperties(prefix = "partners.names")
+	public Map<String,String> PartnersNames(){
+		return new HashMap<>();
+	}
+
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(LogsAopApplication.class, args);
 	}
+
 
 }
