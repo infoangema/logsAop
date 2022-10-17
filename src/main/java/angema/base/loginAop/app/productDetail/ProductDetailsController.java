f0e8cfd5096e674bf0cf0c975316b9cd5b2b2c4e
infoangema <info@angema.com.ar>
2022-10-17 16:09:59 -0300

    [v.0][t.5.3][CHANGE][ECOMMERCE](GERD): - Evolutivo de detalle de productos.

--- src/main/java/angema/base/loginAop/app/productDetail/ProductDetailsController.java
+++ src/main/java/angema/base/loginAop/app/productDetail/ProductDetailsController.java
@@ -22,9 +22,36 @@ public class ProductDetailsController {
         List<ProductDetail>detalleProducto=null;
         try{
             detalleProducto=productDetailService.getDetailProductByPartner(cuit_socio,id_producto);
+            //todo completar items seccion uno
+            //todo completar items seccion dos
         }catch(Exception e){
             logger.info("Exception PartnerThemeController getTematicaBySocio: " + e.getMessage());
         }
         return detalleProducto;
     }
 }
+
+//// product detail
+//{
+//        "id": "321",
+//        "id_producto": "3231",
+//        "cuit_socio":  "30687310434",
+//        "seccion_cero_titulo": "Salud",
+//        "seccion_cero_descripcion": "Seguro de Salud que te brinda servicios de emergencias médicas y protección ante internaciones o enfermedades",
+//        "seccion_cero_subtitulo": "Coberturas desde",
+//        "seccion_cero_precio_base":  "600",
+//
+//        "seccion_uno_descripcion": "¡Aprovechá esta oportunidad!",
+//        "seccion_uno_descripcion_dos": "Contratá tu cobertura hoy de forma 100% online, fácil y segura.",
+//        // product_item
+//        "seccion_uno_items": ["Indemnización ante diagnóstico de cáncer", "Servicio de emergencias y urgencias médicas", "Renta diaria en caso de internación por accidente o enfermedad"],
+//        "seccion_dos_titulo":  "Un seguro que te brinda tranquilidad y protección",
+//        "seccion_dos_descripcion":  "Este seguro te brinda un respaldo económico para afrontar los gastos imprevistos que se puedan originar por el diagnóstico de cáncer o por internaciones prolongadas.",
+//        "seccion_dos_sub_titulo":  "¿QUÉ CUBRE ESTE PRODUCTO?"
+//        // product_item
+//        "seccion_dos_items":  ["Servicio de emergencias, urgencias y consultas médicas a domicilio", "Renta diaria en caso de internación por accidente o enfermedad - : La Compañía abonará al asegurado la suma de cinco mil setecientos cuarenta ($5.740) pesos por cada día de internación en un Establecimiento Asistencial, por un plazo máximo de 90 días.", "Indemnización ante el diagnóstico de cáncer para afrontar aquellos gastos imprevistos"]
+//        }
+//
+//// producto_item
+//// select descripcion from producto_item where id_producto= ? and cuit_socio= ? and seccion = "UNO";
+//        id, id_producto, cuit_socio, seccion (UNO | DOS), descripcion
