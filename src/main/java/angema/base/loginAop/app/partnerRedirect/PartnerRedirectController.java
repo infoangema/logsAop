8d8731c57e671cebd9368400a8ae49535680948b
Fer-Barragan <barragan.fer10@gmail.com>
2022-10-17 09:37:52 -0300

    [v.0][t.9.0][FEAT][ECOMMERCE](FERBA): funcion que te redirecciona a la pagina de la app si el socio coincide con las constantes que se hicieron en el properties

--- src/main/java/angema/base/loginAop/app/partnerRedirect/PartnerRedirectController.java
+++ src/main/java/angema/base/loginAop/app/partnerRedirect/PartnerRedirectController.java
@@ -1,5 +1,6 @@
 package angema.base.loginAop.app.partnerRedirect;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,20 +9,27 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/landing")
 public class PartnerRedirectController {
 
+    @Autowired
+    private final  Map<String,String> PartnersNames =null;
+
     @GetMapping("/{socio}")
-    public void getTematica(@PathVariable String socio, HttpServletResponse response) throws IOException {
+    public void getRedirectByPartnerName(@PathVariable String socio, HttpServletResponse response) throws IOException {
         Cookie cookie = new Cookie("cuit_socio", "30697265895");
         cookie.setMaxAge(60 * 60);
 //        cookie.setSecure(true);
 //        cookie.setHttpOnly(true);
         cookie.setPath("/");
+        if (PartnersNames.containsKey(socio)){
+            response.addCookie(cookie);
+            response.sendRedirect("https://angular-base-login-s6gd-b8m3etfn7-infoangema.vercel.app/dashboard/home");
+        }
 
-        response.addCookie(cookie);
-        response.sendRedirect("https://angular-base-login-s6gd-b8m3etfn7-infoangema.vercel.app/dashboard/home");
+
     }
 }
