8b7c4a910ac2b766f4a4e46af49319af96d8fdc9
Your Name <yourname@example.com>
2022-11-07 19:58:33 -0300

    [v.0][CHANGE][ECOMMERCE](GERR): - Cambios de DAO a ENTITY - rename de endpoint redirect a /site

--- src/main/java/angema/base/loginAop/app/partnerThemes/PartnerTheme.java
+++ src/main/java/angema/base/loginAop/app/partnerThemes/PartnerTheme.java
@@ -3,11 +3,17 @@ package angema.base.loginAop.app.partnerThemes;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Data
 @NoArgsConstructor
+@Entity(name = "partner_themes")
 public class PartnerTheme {
+    @Id
     private Integer id;
     private String cuitSocio;
+    private String nombre;
     private String colorPrimary;
     private String colorSecundary;
     private String colorTertiary;
