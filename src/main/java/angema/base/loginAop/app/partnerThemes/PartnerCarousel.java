9197ff1ad352098858f61ef5cb239d75a4a07da2
Fer-Barragan <barragan.fer10@gmail.com>
2022-11-15 17:52:01 -0300

    [v.0][t.10.0][FEAT][ECOMMERCE](FERBA): Partner carousel

---
+++ src/main/java/angema/base/loginAop/app/partnerThemes/PartnerCarousel.java
@@ -0,0 +1,21 @@
+package angema.base.loginAop.app.partnerThemes;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@Entity(name = "partner_carousel")
+public class PartnerCarousel {
+    @Id
+    private int id;
+    private String nombreCarousel;
+    private String url;
+    private boolean estado;
+    private String createdAt;
+    private String updatedAt;
+    private int partnerThemesId;
+}
