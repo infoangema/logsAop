7b19266e62eb155ddbd7ee7fcc3831e0d8f7db71
Fer-Barragan <barragan.fer10@gmail.com>
2022-11-15 17:53:32 -0300

    [v.0][t.10.0][CHANGE][ECOMMERCE](FERBA): Modificación en partner themes se extrae una lista de propiedades de carousel y otra de colores

--- src/main/java/angema/base/loginAop/app/partnerThemes/PartnerTheme.java
+++ src/main/java/angema/base/loginAop/app/partnerThemes/PartnerTheme.java
@@ -5,29 +5,30 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Transient;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @Entity(name = "partner_themes")
 public class PartnerTheme {
     @Id
-    private Integer id;
+    private int id;
     private String cuitSocio;
     private String nombre;
-    private String colorPrimary;
-    private String colorSecundary;
-    private String colorTertiary;
     private String logoUrl;
-    private String carouselImage1;
-    private String carouselImage2;
-    private String carouselImage3;
-    private String carouselImage4;
     private String bgImageUrl;
     private boolean popUpShow;
     private String popUpTitle;
     private String popUpBody;
     private String popUpFooter;
     private String popUpImageUrl;
+@Transient
+    private List<PartnerCarousel> carouselList;
+    @Transient
+
+    private List<PartnerColor> colorList;
+
 
 
 }
