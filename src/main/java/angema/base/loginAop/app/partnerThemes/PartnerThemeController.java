2328dd7568cc3c30dc4b3d4f2f7df769b406d31f
infoangema <info@angema.com.ar>
2022-12-01 14:45:34 -0300

    [v.0][CHANGE][ECOMMERCE](GERD): - Cambios para fonts dinamicas

--- src/main/java/angema/base/loginAop/app/partnerThemes/PartnerThemeController.java
+++ src/main/java/angema/base/loginAop/app/partnerThemes/PartnerThemeController.java
@@ -34,64 +34,68 @@ public class PartnerThemeController {
 
     @GetMapping("/getThemeBycuit/{cuitSocio}")
     public PartnerTheme getThemeAndColorAndCarouselBycuit(@PathVariable String cuitSocio) {
-        PartnerTheme tematica=null;
-        try{
-            tematica=partnerThemeService.getThemeAndColorAndCarouselBycuit(cuitSocio);
-        }catch(Exception e){
+        PartnerTheme tematica = null;
+        try {
+            tematica = partnerThemeService.getThemeAndColorAndCarouselBycuit(cuitSocio);
+        } catch (Exception e) {
             logger.info("Exception PartnerThemeController getTematicaBySocio: " + e.getMessage());
         }
         return tematica;
     }
 
 
-    @GetMapping(value="/getLogoByCuit/{cuitSocio}",produces = MediaType.IMAGE_PNG_VALUE)
+    @GetMapping(value = "/getLogoByCuit/{cuitSocio}", produces = MediaType.IMAGE_PNG_VALUE)
     @ResponseBody
-    public  byte[] getLogo(@PathVariable String cuitSocio) throws IOException {
-        InputStream in = PartnerThemeController.class.getResourceAsStream("/static/"+cuitSocio+"/images/logo-"+cuitSocio+".png");
+    public byte[] getLogo(@PathVariable String cuitSocio) throws IOException {
+        InputStream in = PartnerThemeController.class.getResourceAsStream("/static/" + cuitSocio + "/images/logo-" + cuitSocio + ".png");
         return IOUtils.toByteArray(in);
     }
 
-    @GetMapping(value="/getCarouselImageByCuit/{cuitSocio}/{numero}",produces = MediaType.IMAGE_PNG_VALUE)
+    @GetMapping(value = "/getCarouselImageByCuit/{cuitSocio}/{numero}", produces = MediaType.IMAGE_PNG_VALUE)
     @ResponseBody
-    public  byte[] getCarouselImageByCuit(@PathVariable String cuitSocio,@PathVariable String numero) throws IOException {
-        InputStream in = PartnerThemeController.class.getResourceAsStream("/static/"+cuitSocio+"/images/carousel_image_"+numero+".jpg");
+    public byte[] getCarouselImageByCuit(@PathVariable String cuitSocio, @PathVariable String numero) throws IOException {
+        InputStream in = PartnerThemeController.class.getResourceAsStream("/static/" + cuitSocio + "/images/carousel_image_" + numero + ".jpg");
         return IOUtils.toByteArray(in);
     }
 
     // TODO: 13/10/2022 :definir imagen background y implementar funcionalidad en front.
-    @GetMapping(value="/getBackGroundImageByCuit/{cuitSocio}",produces = MediaType.IMAGE_PNG_VALUE)
+    @GetMapping(value = "/getBackGroundImageByCuit/{cuitSocio}", produces = MediaType.IMAGE_PNG_VALUE)
     @ResponseBody
-    public  byte[] getBackGroundImageByCuit(@PathVariable String cuitSocio) throws IOException {
-        InputStream in = PartnerThemeController.class.getResourceAsStream("/static/images/bg-image-"+cuitSocio+".jpg");
+    public byte[] getBackGroundImageByCuit(@PathVariable String cuitSocio) throws IOException {
+        InputStream in = PartnerThemeController.class.getResourceAsStream("/static/images/bg-image-" + cuitSocio + ".jpg");
         return IOUtils.toByteArray(in);
     }
 
 
-    @GetMapping(value="/getCoberturaByCuitAndProductId/{cuitSocio}/{id_producto}",produces = MediaType.APPLICATION_PDF_VALUE)
+    @GetMapping(value = "/getCoberturaByCuitAndProductId/{cuitSocio}/{id_producto}", produces = MediaType.APPLICATION_PDF_VALUE)
     @ResponseBody
-    public  byte[] getCoberturaByCuitAndProductId(@PathVariable String cuitSocio,@PathVariable String id_producto) throws IOException {
-        InputStream in = PartnerThemeController.class.getResourceAsStream("/static/"+cuitSocio+"/productos/coberturaProduct/"+id_producto+".pdf");
-        return IOUtils.toByteArray(in);
-    }
-    @GetMapping(value="/getCondicionesByCuitAndProductId/{cuitSocio}/{id_producto}",produces = MediaType.APPLICATION_PDF_VALUE)
-    @ResponseBody
-    public  byte[] getCondicionesByCuitAndProductId(@PathVariable String cuitSocio,@PathVariable String id_producto) throws IOException {
-        InputStream in = PartnerThemeController.class.getResourceAsStream("/static/"+cuitSocio+"/productos/basesYCondicionesProduct/"+id_producto+".pdf");
+    public byte[] getCoberturaByCuitAndProductId(@PathVariable String cuitSocio, @PathVariable String id_producto) throws IOException {
+        InputStream in = PartnerThemeController.class.getResourceAsStream("/static/" + cuitSocio + "/productos/coberturaProduct/" + id_producto + ".pdf");
         return IOUtils.toByteArray(in);
     }
 
-
-
-
-    @GetMapping(value="/getFontByCuit/{cuitSocio}/primary/type/font_primary.ttf",produces = "application/x-font-ttf")
+    @GetMapping(value = "/getCondicionesByCuitAndProductId/{cuitSocio}/{id_producto}", produces = MediaType.APPLICATION_PDF_VALUE)
     @ResponseBody
-    public  byte[] getFontByCuit(@PathVariable String cuitSocio) throws IOException {
-        InputStream in=null;
-        if (cuitSocio.equals(cuitCarre))
-            in = PartnerThemeController.class.getResourceAsStream("/static/fonts/font-"+cuitSocio+"/font_primary_"+cuitSocio+".ttf");
-        else
-            in = PartnerThemeController.class.getResourceAsStream("/static/fonts/font-"+cuitSocio+"/static/RobotoSlab-Regular.ttf");
+    public byte[] getCondicionesByCuitAndProductId(@PathVariable String cuitSocio, @PathVariable String id_producto) throws IOException {
+        InputStream in = PartnerThemeController.class.getResourceAsStream("/static/" + cuitSocio + "/productos/basesYCondicionesProduct/" + id_producto + ".pdf");
+        return IOUtils.toByteArray(in);
+    }
 
+
+    @GetMapping(value = "/getFontByCuit/{cuitSocio}/primary/type/font_primary.ttf", produces = "application/x-font-ttf")
+    @ResponseBody
+    public byte[] getFontByCuit(@PathVariable String cuitSocio) throws IOException {
+        InputStream in = null;
+        in = PartnerThemeController.class.getResourceAsStream("/static/" + cuitSocio + "/fonts/font_primary_" + cuitSocio + ".ttf");
+        return IOUtils.toByteArray(in);
+
+
+    }
+    @GetMapping(value = "/getFontByCuit/{cuitSocio}/primary/type/font_primary.css", produces = "test/css")
+    @ResponseBody
+    public byte[] getFontCssByCuit(@PathVariable String cuitSocio) throws IOException {
+        InputStream in = null;
+        in = PartnerThemeController.class.getResourceAsStream("/static/" + cuitSocio + "/fonts/font_primary_" + cuitSocio + ".css");
         return IOUtils.toByteArray(in);
 
 
