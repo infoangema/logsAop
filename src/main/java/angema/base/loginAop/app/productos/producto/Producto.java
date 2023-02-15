b77032e4fffb0b2979492ba11549f16a02bf9c1a
infoangema <info@angema.com.ar>
2023-02-15 20:30:07 -0300

    [CODE][ECOMMERCE](GERR): - Agregado validaciones de Spring Validation. - Mejoras de excepciones de Producto en controller. Se quitaron los catch del controlador.

--- src/main/java/angema/base/loginAop/app/productos/producto/Producto.java
+++ src/main/java/angema/base/loginAop/app/productos/producto/Producto.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import javax.persistence.*;
 
 @ToString
@@ -14,14 +16,23 @@ import javax.persistence.*;
 @Table(uniqueConstraints={@UniqueConstraint(columnNames = {"idProducto" , "cuitSocio"})})
 public class Producto {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "producto_id_seq")
+    @SequenceGenerator(name = "producto_id_seq", sequenceName = "producto_id_seq")
     public Integer id;
+
     @NotNull
     public String idProducto;
+
     @NotNull
     public String cuitSocio;
+
+    @Size(min = 4, max = 43, message = "El titulo debe contener entre 4 y 43 caracteres.")
+    @NotBlank(message = "El titulo no puede estar nulo o vacio.")
     public String titulo;
+
     public String slogan;
+
     public String urlImagen;
+
     public String urlImagenPrecio;
 }
--- src/main/java/angema/base/loginAop/app/productos/producto/ProductoService.java
+++ src/main/java/angema/base/loginAop/app/productos/producto/ProductoService.java
@@ -1,45 +1,51 @@
 package angema.base.loginAop.app.productos.producto;
 
-import angema.base.loginAop.app.productos.detalles.DetalleRepository;
-import angema.base.loginAop.app.temas.carrusel.Carrusel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionSystemException;
 
+import javax.persistence.RollbackException;
+import javax.validation.ConstraintViolationException;
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
+import static angema.base.loginAop.core.exceptions.ExceptionService.getErrorMessage;
 
 @Service
 public class ProductoService {
 
     @Autowired
     private ProductoRepository productoRepository;
-    @Autowired
-    private DetalleRepository detalleRepository;
 
     public Producto buscarProductoPorId_y_CuitSocio(String cuitSocio, String productoId) {
         try {
             Producto prd =  productoRepository.findByCuitSocioAndIdProducto(cuitSocio, productoId);
             return prd;
         } catch (Exception e) {
-            throw new ProductoException(e.getMessage());
+            throw new ProductoException("Error al intentar obtner detalles del producto -> " + productoId + ": " + e.getMessage());
         }
     }
 
     public void addProducto(List<Producto> productoList) {
         try {
             productoRepository.saveAll(productoList);
+        } catch (ConstraintViolationException e) {
+            throw new ProductoException("Error al intentar guardar producto: " + getErrorMessage(e));
+        } catch (DataIntegrityViolationException e) {
+            throw new ProductoException("Error al intentar guardar producto: " + e.getRootCause().getMessage());
         } catch (Exception e) {
-            throw new ProductoException(e.getMessage());
+            throw new ProductoException("Error al intentar guardar producto: " + e.getMessage());
         }
     }
 
     public List<Producto> getProducto(String cuitSocio) {
         try {
-            List<Producto> productoList = productoRepository.findByCuitSocio(cuitSocio);
-            return productoList;
+            return productoRepository.findByCuitSocio(cuitSocio);
         } catch (Exception e) {
-            throw new ProductoException(e.getMessage());
+            throw new ProductoException("Error al intentar obtener producto cuit -> " + cuitSocio + ": " + e.getMessage());
         }
     }
 
@@ -51,45 +57,62 @@ public class ProductoService {
                 }
             }
             productoRepository.saveAll(productoList);
+        } catch (ConstraintViolationException e) {
+            throw new ProductoException("Error al intentar actualizar producto: " + getErrorMessage(e));
+        } catch (DataIntegrityViolationException e) {
+            throw new ProductoException("Error al intentar actualizar producto: " + e.getRootCause().getMessage());
         } catch (Exception e) {
-            throw new ProductoException(e.getMessage());
+            if(((TransactionSystemException) e).getOriginalException().getCause() instanceof ConstraintViolationException) {
+                RollbackException ex = (RollbackException) e.getCause();
+                ConstraintViolationException ex2 = (ConstraintViolationException) ex.getCause();
+                throw new ProductoException("Error al intentar actualizar producto: " + getErrorMessage(ex2));
+            }
+            throw new ProductoException("Error al intentar actualizar producto: " + e.getMessage());
         }
     }
 
-    public Producto getProductoFromUpdateParams(Map<String, Object> fields) throws IllegalAccessException, NoSuchFieldException{
-        Producto updateDto = new Producto();
-        Class<?> updateDtoClass = updateDto.getClass();
-        for (Map.Entry<String, Object> entry : fields.entrySet()) {
-            try {
-                Field field = updateDtoClass.getDeclaredField(entry.getKey());
-                field.setAccessible(true);
-                if (field.getType().getName().equals("java.lang.Integer")) {
-                    field.set(updateDto, Integer.parseInt(entry.getValue().toString().trim()));
-                } else {
-                    field.set(updateDto, entry.getValue());
+    public Producto getProductoFromUpdateParams(Map<String, Object> fields) {
+        try {
+            Producto updateDto = new Producto();
+            Class<?> updateDtoClass = updateDto.getClass();
+            for (Map.Entry<String, Object> entry : fields.entrySet()) {
+                try {
+                    Field field = updateDtoClass.getDeclaredField(entry.getKey());
+                    field.setAccessible(true);
+                    if (field.getType().getName().equals("java.lang.Integer")) {
+                        field.set(updateDto, Integer.parseInt(entry.getValue().toString().trim()));
+                    } else {
+                        field.set(updateDto, entry.getValue());
+                    }
+                } catch (NoSuchFieldException e) {
+                    return null;
+                } catch (IllegalAccessException e) {
+                    throw new ProductoException(e.getMessage());
                 }
-            } catch (NoSuchFieldException e) {
-                return null;
-            } catch (IllegalAccessException e) {
-                throw new ProductoException(e.getMessage());
             }
-        }
-        Producto producto = productoRepository.findById(updateDto.getId()).get();
-        Class<?> coberturaClass = producto.getClass();
-        for (Field field : updateDtoClass.getDeclaredFields()) {
-            field.setAccessible(true);
-            Object value = field.get(updateDto);
-            if (value != null) {
-                Field coberturaField = coberturaClass.getDeclaredField(field.getName());
-                coberturaField.setAccessible(true);
-                coberturaField.set(producto, value);
+            Producto producto = productoRepository.findById(updateDto.getId()).get();
+            Class<?> coberturaClass = producto.getClass();
+            for (Field field : updateDtoClass.getDeclaredFields()) {
+                field.setAccessible(true);
+                Object value = field.get(updateDto);
+                if (value != null) {
+                    Field coberturaField = coberturaClass.getDeclaredField(field.getName());
+                    coberturaField.setAccessible(true);
+                    coberturaField.set(producto, value);
+                }
             }
+            return producto;
+        } catch (Exception e) {
+            throw new ProductoException("Error al intentar modificar producto: " + e.getMessage());
         }
-        return producto;
     }
 
     public void deleteProducto(Integer id) {
-        Producto producto = productoRepository.findById(id).get();
-        productoRepository.delete(producto);
+        try {
+            Producto producto = productoRepository.findById(id).get();
+            productoRepository.delete(producto);
+        } catch (Exception e) {
+            throw new ProductoException("Error al intentar eliminar producto: " + e.getMessage());
+        }
     }
 }
